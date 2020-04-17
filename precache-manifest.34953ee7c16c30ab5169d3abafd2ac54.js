2505b53d63f0ca4ca9aad720d44d92f20ec709bc
Ruturaj Jadeja <ruturajjadeja21@gmail.com>
2020-04-17 18:34:26 -0400

    Updates

---
+++ precache-manifest.34953ee7c16c30ab5169d3abafd2ac54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed619590758c928ded607ca266f43593",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "aa0f54bd8cf26e938038",
+    "url": "/COVID-19-Tracker/static/css/main.4269b9ea.chunk.css"
+  },
+  {
+    "revision": "c88b3d267a156c5359b9",
+    "url": "/COVID-19-Tracker/static/js/2.17dbca3c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.17dbca3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0f54bd8cf26e938038",
+    "url": "/COVID-19-Tracker/static/js/main.43906bee.chunk.js"
+  },
+  {
+    "revision": "1efff0d3dddd84216458",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.67428a36.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
